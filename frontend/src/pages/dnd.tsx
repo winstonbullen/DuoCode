2c82409b134b0f4daa97673782e810e42c1a19a1
Jason Xu <jaxu@uw.edu>
2023-05-02 18:36:02 -0700

    Style changes

--- frontend/src/pages/dnd.tsx
+++ frontend/src/pages/dnd.tsx
@@ -4,8 +4,7 @@ const Dnd = () => {
     return (
         <div>
             <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
+                Drag and Drop goes here
             </h1>
         </div>
     );
