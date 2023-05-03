ec6388e24a8b6564641c1251945a38be71c7a90a
Jason Hua <jhua04@cs.washington.edu>
2023-05-02 17:49:04 -0700

    Added Test for multiple choice

--- frontend/src/test/shortanswer.test.tsx
+++ frontend/src/test/shortanswer.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import ShortAnswer from '../pages/shortanswer';
+import Multiplechoice from "../pages/multiplechoice";
 
 // Mock the quiz data
 jest.mock('../api/quiz', () => ({
@@ -11,6 +12,11 @@ jest.mock('../api/quiz', () => ({
                 prompt: 'Create a variable named foo of type int and initialize it to 1 using the code box below.',
                 correct_answer: 'int foo = 1;',
             },
+            {
+                prompt: "What is 2 + 2?",
+                correct_answer: "4",
+                distractors: ["1", "2", "3"]
+            }
         ],
     },
 }));
@@ -23,4 +29,13 @@ describe('ShortAnswer', () => {
         );
         expect(question).toBeInTheDocument();
     });
+    describe('MultipleChoice', () => {
+        it('renders the correct question prompt', () => {
+            render(<Multiplechoice/>);
+            const question = screen.getByLabelText(
+                'What is 2 + 2?'
+            );
+            expect(question).toBeInTheDocument();
+        });
+    })
 });
