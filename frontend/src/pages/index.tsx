87ae248579bfe6a9dc67b0f1b5c1103451be9aeb
Jason Xu <jaxu@uw.edu>
2023-05-08 11:12:55 -0700

    Updated homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,89 @@
-import { Button} from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './index.css';
 
+type Language = 'Java' | 'Python';
 
-const Home = () => {
+const HomePage: React.FC = () => {
+    const [language, setLanguage] = useState<Language>('Java');
+    const [dailyChallengeProgress, setDailyChallengeProgress] = useState<number>(70);
+
+    const handleLanguageChange = (selectedLanguage: Language) => {
+        setLanguage(selectedLanguage);
+    };
+
     return (
-        <div>
-            <h1>Welcome to DuoCode</h1>
-            <Link to="/multiplechoice">
-                <Button variant="outlined">
-                    Start
-                </Button>
-            </Link>
+        <div className="home-page-container">
+            <div className="navbar">
+                <div className="duocode-title">
+                    <h2>DuoCode</h2>
+                </div>
+                <div className="navbar-links">
+                    <Link to="/">Learn</Link>
+                    <Link to="/">Shop</Link>
+                    <Link to="/">Profile</Link>
+                    <Link to="/">More</Link>
+                </div>
+                <div className="user-info">
+                    <img src="profile-pic.png" alt="Profile" className="profile-pic" />
+                    <p className="username">John Doe</p>
+                </div>
+            </div>
+            <div className="unit-container">
+                <div className="unit">
+                    <h3>Unit 1</h3>
+                    <p>Variables and Resources</p>
+                </div>
+                <div className="checkpoints">
+                    <div className="checkpoint complete">
+                        <Link to='/question'>1</Link>
+                    </div>
+                    <div className="checkpoint">
+                        <span>2</span>
+                    </div>
+                    <div className="checkpoint">
+                        <span>3</span>
+                    </div>
+                    <div className="checkpoint">
+                        <span>4</span>
+                    </div>
+                </div>
+            </div>
+            <div className="unit-container">
+                <div className="unit">
+                    <h3>Unit 2</h3>
+                    <p>Method Headers and Resources</p>
+                </div>
+                <div className="checkpoints">
+                    <div className="checkpoint">
+                        <span>1</span>
+                    </div>
+                    <div className="checkpoint">
+                        <span>2</span>
+                    </div>
+                    <div className="checkpoint">
+                        <span>3</span>
+                    </div>
+                    <div className="checkpoint">
+                        <span>4</span>
+                    </div>
+                </div>
+            </div>
+            <div className="language-dropdown">
+                <select value={language} onChange={(e) => handleLanguageChange(e.target.value as Language)}>
+                    <option value="Java">Java</option>
+                    <option value="Python">Python</option>
+                </select>
+            </div>
+            <div className="daily-challenge">
+                <p>Daily Challenge</p>
+                <div className="progress-bar">
+                    <div className="progress" style={{ width: `${dailyChallengeProgress}%` }}></div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Home;
+export default HomePage;
