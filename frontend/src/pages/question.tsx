31f541fe23b94e44d7f326dd270d0d612b20ce59
evankim <ekim03@uw.edu>
2023-05-07 19:35:03 -0700

    some more edits to q

--- frontend/src/pages/question.tsx
+++ frontend/src/pages/question.tsx
@@ -9,20 +9,25 @@ interface QuestionProps {}
 
 const Question: React.FC<QuestionProps> = () => {
     return (
-        <>
-        <div className='question-header'>
-            <div className='btn'>
-                <CloseBtn/>
+        <div className='page'>
+            <div className='question-header'>
+                <div className='btn'>
+                    <CloseBtn/>
+                </div>
+                <div className='btn'>
+                    <SettingBtn/>
+                </div>
+                <div className='btn'>
+                    <ProgressBar completed={20}/>
+                </div>
             </div>
-            <div className='btn'>
-                <SettingBtn/>
+            <div className='question-content'>
+                
             </div>
-            <div className='btn'>
-                <ProgressBar completed={20}/>
+            <div className="question-footer">
+                <hr></hr>
             </div>
-
         </div>
-        </>
     );
 };
 
