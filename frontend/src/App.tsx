3dfcb8b7b0e46a19c29b02659f2d8c1bbb2c3f2d
jgao2 <jgao222@uw.edu>
2023-05-14 17:22:34 -0700

    revert modules change that broke the build, add build step to CI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components';
+// import Navbar from './components';
 import { BrowserRouter as Router, Routes, Route}
   from 'react-router-dom';
 import Home from './pages';
-import Choice from './pages/multiplechoice';
-import ShortAnswer from "./pages/shortanswer";
-import DragDrop from "./pages/dnd";
-import Question from "./pages/question";
+// import Choice from './pages/multiplechoice';
+// import ShortAnswer from "./pages/shortanswer";
+// import DragDrop from "./pages/";
+import Question from "./pages";
 
 function App() {
     return (
