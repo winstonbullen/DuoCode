126c84b3b19d5e198d7d080da07565c21cb12823
JP <johnpbg@outlook.com>
2023-05-15 03:20:29 -0700

    homepage without svgs

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,15 +7,116 @@ type Language = 'Java' | 'Python';
 
 const HomePage: React.FC = () => {
     const [language, setLanguage] = useState<Language>('Java');
-    // const [dailyChallengeProgress, setDailyChallengeProgress] = useState<number>(70);
-    const [dailyChallengeProgress, ] = useState<number>(70);
+    const [dailyChallengeProgress, setDailyChallengeProgress] = useState<number>(70);
 
     const handleLanguageChange = (selectedLanguage: Language) => {
         setLanguage(selectedLanguage);
     };
 
     return (
-        <div className="home-page-container">
+        <div className="container">
+            <div className="sidebar">
+                <div className="duocode-title">
+                    <h2>DuoCode</h2>
+                </div>
+                <div className="navbar-links">
+                    <ul>
+                        <li>
+                        <Link to="/">
+                        <img src="learn.svg" alt="learn" />Learn</Link>
+                        </li>
+                        <li>
+                        <Link to="/">
+                        <img src="shop.svg" alt="shop" />Shop</Link>
+                        </li>
+                        <li>
+                        <Link to="/">
+                        <img src="profile.svg" alt="profile" />Profile</Link>
+                        </li>
+                        <li>
+                        <Link to="/">
+                        <img src="more.svg" alt="more" />More</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="account">
+                    <h3><img src="account.svg" alt="account" />CoolCoder123</h3>
+                </div>
+            </div>
+            <div className="grayline" style={{ background: 'lightgrey', width: '.3vh', marginLeft: '24.7%', height: '100vh' }}>
+            </div>
+            <div className="middlepane">
+            <div className="unit1-container">
+                <div className="unit1">
+                    <h3>Unit 1</h3>
+                    <p className="resources">Resources</p>
+                    <p className="variables">Variables</p>
+                </div>
+                <div className="checkpoints">
+                    <div className="checkpoint1">
+                        <Link to='/question'>
+                        <img src="whitestar.svg" alt="star" /></Link>
+                    </div>
+                    <div className="checkpoint2">
+                        <Link to='/'>
+                        <img src="lock.svg" alt="lock" /></Link>
+                    </div>
+                    <div className="checkpoint3">
+                        <Link to='/'>
+                        <img src="lock.svg" alt="lock" /></Link>
+                    </div>
+                    <div className="checkpoint4">
+                        <Link to='/'>
+                        <img src="flag.svg" alt="flag" /></Link>
+                    </div>
+                </div>
+            </div>
+            <div className="unit2-container">
+                <div className="unit2">
+                    <h3>Unit 2</h3>
+                    <p className="resources">Resources</p>
+                    <p className="method-headers">Method Headers</p>
+                </div>
+                <div className="checkpoints">
+                    <div className="checkpoint1">
+                        <Link to='/'>
+                        <img src="blackstar.svg" alt="star" /></Link>
+                    </div>
+                    <div className="checkpoint2">
+                        <Link to='/'>
+                        <img src="lock.svg" alt="lock" /></Link>
+                    </div>
+                    <div className="checkpoint3">
+                        <Link to='/'>
+                        <img src="lock.svg" alt="lock" /></Link>
+                    </div>
+                    <div className="checkpoint4">
+                        <Link to='/'>
+                        <img src="flag.svg" alt="flag" /></Link>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="rightpane">
+            <div className="language">
+                <img src="java.svg" alt="java" />
+                <h3 className="language-name">Java</h3>
+            </div>
+            <div className="daily-challenges">
+                <h3>Daily Challenges</h3>
+                <h4>Progress</h4>
+                <div className="progress-bar">
+                    <div>1/3</div>
+                    <img className="coin-img" src="coin.svg" alt="coin" />
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+/*
+ <div className="home-page-container">
             <div className="navbar">
                 <div className="duocode-title">
                     <h2>DuoCode</h2>
@@ -27,7 +128,7 @@ const HomePage: React.FC = () => {
                     <Link to="/">More</Link>
                 </div>
                 <div className="user-info">
-                    <img src="profile-pic.png" alt="User Profile" className="profile-pic" />
+                    <img src="profile-pic.png" alt="Picture" className="profile-pic" />
                     <p className="username">John Doe</p>
                 </div>
             </div>
@@ -84,7 +185,6 @@ const HomePage: React.FC = () => {
                 </div>
             </div>
         </div>
-    );
-};
+*/
 
 export default HomePage;
