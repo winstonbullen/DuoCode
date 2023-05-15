82288f43cd09036ba1d7d0df82c7fb2a081065c7
JP <johnpbg@outlook.com>
2023-05-15 05:16:02 -0700

    Made a functional homepage with pngs

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -23,24 +23,28 @@ const HomePage: React.FC = () => {
                     <ul>
                         <li>
                         <Link to="/">
-                        <img src="learn.svg" alt="learn" />Learn</Link>
+                        <img src={require("./images/learn.png")} alt="learn" /></Link>
+                        <p>Learn</p>
                         </li>
                         <li>
                         <Link to="/">
-                        <img src="shop.svg" alt="shop" />Shop</Link>
+                        <img src={require("./images/shop.png")} alt="shop" /></Link>
+                        <p>Shop</p>
                         </li>
                         <li>
                         <Link to="/">
-                        <img src="profile.svg" alt="profile" />Profile</Link>
+                        <img src={require("./images/profile.png")} alt="profile" /></Link>
+                        <p>Profile</p>
                         </li>
                         <li>
                         <Link to="/">
-                        <img src="more.svg" alt="more" />More</Link>
+                        <img src={require("./images/more.png")} alt="more" /></Link>
+                        <p>More</p>
                         </li>
                     </ul>
                 </div>
                 <div className="account">
-                    <h3><img src="account.svg" alt="account" />CoolCoder123</h3>
+                    <h3><img src={require("./images/account.png")} alt="account" />CoolCoder123</h3>
                 </div>
             </div>
             <div className="grayline" style={{ background: 'lightgrey', width: '.3vh', marginLeft: '24.7%', height: '100vh' }}>
@@ -55,19 +59,19 @@ const HomePage: React.FC = () => {
                 <div className="checkpoints">
                     <div className="checkpoint1">
                         <Link to='/question'>
-                        <img src="whitestar.svg" alt="star" /></Link>
+                        <img src={require("./images/whitestar.png")} alt="whitestar" /></Link>
                     </div>
                     <div className="checkpoint2">
                         <Link to='/'>
-                        <img src="lock.svg" alt="lock" /></Link>
+                        <img src={require("./images/lock.png")} alt="lock" /></Link>
                     </div>
                     <div className="checkpoint3">
                         <Link to='/'>
-                        <img src="lock.svg" alt="lock" /></Link>
+                        <img src={require("./images/lock.png")} alt="lock" /></Link>
                     </div>
                     <div className="checkpoint4">
                         <Link to='/'>
-                        <img src="flag.svg" alt="flag" /></Link>
+                        <img src={require("./images/flag.png")} alt="flag" /></Link>
                     </div>
                 </div>
             </div>
@@ -80,34 +84,34 @@ const HomePage: React.FC = () => {
                 <div className="checkpoints">
                     <div className="checkpoint1">
                         <Link to='/'>
-                        <img src="blackstar.svg" alt="star" /></Link>
+                        <img src={require("./images/blackstar.png")} alt="blackstar" /></Link>
                     </div>
                     <div className="checkpoint2">
                         <Link to='/'>
-                        <img src="lock.svg" alt="lock" /></Link>
+                        <img src={require("./images/lock.png")} alt="lock" /></Link>
                     </div>
                     <div className="checkpoint3">
                         <Link to='/'>
-                        <img src="lock.svg" alt="lock" /></Link>
+                        <img src={require("./images/lock.png")} alt="lock" /></Link>
                     </div>
                     <div className="checkpoint4">
                         <Link to='/'>
-                        <img src="flag.svg" alt="flag" /></Link>
+                        <img src={require("./images/flag.png")} alt="flag" /></Link>
                     </div>
                 </div>
             </div>
             </div>
             <div className="rightpane">
             <div className="language">
-                <img src="java.svg" alt="java" />
+                <img src={require("./images/java.png")} alt="java" />
                 <h3 className="language-name">Java</h3>
             </div>
             <div className="daily-challenges">
                 <h3>Daily Challenges</h3>
                 <h4>Progress</h4>
                 <div className="progress-bar">
-                    <div>1/3</div>
-                    <img className="coin-img" src="coin.svg" alt="coin" />
+                    <div>0/3</div>
+                    <img src={require("./images/coin.png")} alt="coin" />
                 </div>
             </div>
         </div>
