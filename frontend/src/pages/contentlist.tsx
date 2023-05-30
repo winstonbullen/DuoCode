3cf39213bc1b4aa97f65db749bea71ee67c7a04e
jgao2 <jgao222@uw.edu>
2023-05-29 19:21:14 -0700

    add all the content units, fix bug w/ case specific language check for rendering

---
+++ frontend/src/pages/contentlist.tsx
@@ -0,0 +1,37 @@
+export const JAVA_CONTENT = [
+  {unitName: "variables",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "operators",
+  resource_link: "https://www.google.com/search?q=operators"},
+  {unitName: "conditions",
+  resource_link: "https://www.google.com/search?q=conditions"},
+  {unitName: "loops",
+  resource_link: "https://www.google.com/search?q=loops"},
+  {unitName: "methods",
+  resource_link: "https://www.google.com/search?q=methods"},
+  {unitName: "arrays",
+  resource_link: "https://www.google.com/search?q=arrays"},
+  {unitName: "collections",
+  resource_link: "https://www.google.com/search?q=collections"},
+  {unitName: "classes",
+  resource_link: "https://www.google.com/search?q=classes"}
+]
+
+export const PYTHON_CONTENT = [
+  {unitName: "variables",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "operators",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "conditions",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "loops",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "functions",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "lists",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "collections",
+  resource_link: "https://www.google.com/search?q=variables"},
+  {unitName: "classes",
+  resource_link: "https://www.google.com/search?q=variables"}
+]
