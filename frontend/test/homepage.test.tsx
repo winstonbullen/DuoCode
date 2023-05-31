2ad51c9871a48a9d526b67cf59fe1c09eecef98c
jgao2 <jgao222@uw.edu>
2023-05-31 15:00:47 -0700

    fix frontend test warnings, and fix the failures that the warning was hiding

--- frontend/test/homepage.test.tsx
+++ frontend/test/homepage.test.tsx
@@ -1,9 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from '../src/App';
+// import App from '../src/App';
+import { BrowserRouter } from 'react-router-dom';
+import HomePage from 'pages';
 
 describe('Homepage', () => {
-    it('should render without error', () => {
-        render(<App />)
+
+    beforeAll(() => {
+        global.fetch = jest.fn((endpoint) => {
+            if (endpoint === "/completion") {
+                return Promise.resolve({
+                    json: () => Promise.resolve([]),
+                })
+            } else if (endpoint === "/userinfo") {
+                return Promise.resolve({
+                    json: () => Promise.resolve({ user: "testUsername" }),
+                })
+            }
+        }) as jest.Mock;
+    });
+
+    it('should render without error', async () => {
+        render(<HomePage />, {wrapper: BrowserRouter});
+        await screen.findByText("testUsername");
     });
 });
