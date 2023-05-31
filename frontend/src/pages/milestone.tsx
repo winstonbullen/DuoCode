6d2716cadad23d4e838d386af0920ffa29cac5a1
Jason Hua <jhua04@cs.washington.edu>
2023-05-30 22:20:47 -0700

    Fixed errors

--- frontend/src/pages/milestone.tsx
+++ frontend/src/pages/milestone.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, {useEffect, useRef, useState } from 'react';
 import CloseBtn from '../components/CloseBtn'
 import ProgressBar from '../components/ProgressBar';
 import ShortAnswer from './shortanswer';
